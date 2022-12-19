fefcea742fe85e57fdfe464f97b95725ddfa4c27
Takenori KUBO <takenori.kubo@quantum.ne.jp>
2022-12-19 19:18:44 +0900

    Add auth

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "awsamplifycognitoreab3c229ca": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import { Amplify } from 'aws-amplify';
+import { withAuthenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+import awsExport from './aws-exports';
 
-function App() {
+Amplify.configure(awsExport);
+
+function App({ signOut, user }) {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h2>Hello React app with AWS</h2>
+        {user? (
+          <>
+            <h3>私は権限を持っています：{user.username}</h3>
+            <button onClick={signOut}>SingOut</button>
+          </>
+        ) : (        
+          <>
+            <h3>私は権限を持っていません</h3>
+          </>
+
+        )}
       </header>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
